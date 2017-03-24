5760b958ad18653f974d0bd546720b2c4dbca9ba
Prima Utama Apriansyah Github UI Design <prima.uidesign@gmail.com>
2017-03-24 08:33:57 +0700

    Tambahan baru Dari theme forest

---
+++ city-listing/assets/js/leaflet-map/sampleMapContactMarker.js
@@ -0,0 +1,9 @@
+var listingItems = [
+    {
+        title: "Our Office",
+        address: "27 Orchard St, New York, NY 10002",
+        lat: 40.715484,
+        lng: -73.99177320000001,
+        img: "assets/images/contact/1.jpg"
+    }
+];
